24292e2f1f4e3da5d15a65a97183281422a629bc
SelimAbdelmonsefBtech <selim.mohamed@btech.com>
2023-10-22 15:04:58 +0300

    applied light and dark theme on tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+export const StyledContent = styled.h1<{ theme: string }>(({ theme }) => ({
+  color:
+    theme === 'light' ? 'text-content-primary' : 'text-content-primary-inverse',
+}));
+
+export const App = () => {
+  const [theme, setTheme] = useState<string>('light');
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  const handleSwitchTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  console.log({ theme });
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TAILWIND TRIAL</h1>
-      </header>
+    <div className="h-screen bg-white dark:bg-black justify-center items-center text-center">
+      <div>
+        <h1
+          className={`${
+            theme === 'light'
+              ? 'text-content-primary'
+              : 'text-content-primary-inverse'
+          }`}
+        >
+          TEXT
+        </h1>
+      </div>
+
+      <div>
+        <button
+          className="bg-green-200 p-4 rounded-3xl text-content-danger-secondary"
+          onClick={handleSwitchTheme}
+        >
+          Change theme
+        </button>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
